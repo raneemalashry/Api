892795570e3389e42821b2532782afe2d8f84e12
Raneem Alashry <raneemalashry@users.noreply.github.com>
2021-01-23 02:45:34 +0200

    “wip”

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  Raneem  yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+                  Raneem 
                 </div>
 
                 <div class="links">
